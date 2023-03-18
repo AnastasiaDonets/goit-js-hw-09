d6d441703459e62ca66dd1377ae05530139ee84f
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-03-19 01:49:52 +0200

    Task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const INTERVAL_DELAY = 1000;
+let colorInterval = null;
+
+refs.btnStart.addEventListener('click', onChangeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function onChangeColor() {
+  colorInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function onBtnStop() {
+  clearInterval(colorInterval);
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+}
